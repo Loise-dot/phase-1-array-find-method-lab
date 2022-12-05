ac19f53a7085646b3da8677be0d6ac77129e83b9
Loise <loise.wambui@student.moringaschool.com>
2022-12-05 22:16:54 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+let records = [
+  { year: 2015, result: "W" },
+  { year: 2014, result: "N/A" },
+];
+let superbowlWin = (records) => {
+  let res = records.find((record) => record.result == "W");
+  if (res) {
+    return res.year;
+  } else {
+    return undefined;
+  }
+};
